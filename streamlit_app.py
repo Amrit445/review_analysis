a61b3fc604e2d8bd14e33bdab3872103ba4a2223
Amritansh Gupta <b521011@iiit-bh.ac.in>
2024-12-23 13:49:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+st.title("Review App")
 url= "https://www.flipkart.com/l-oral-paris-extraordinary-oil-shampoo-340-ml-conditioner-180-serum-30/p/itm0a099856ea57f?pid=CBKGZ5EZ6EHWJ23H&ssid=pntzsmsrps0000001734624826105"
 
 r=requests.get(url)
-st.
+st.write(r)
+
+
